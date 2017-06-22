4299fb7ad465abaa3a02b1470b75531439f2fd7e
rajashekhar <shekhar.yedla@gmail.com>
2017-06-22 11:14:09 +0530

    testcases

--- src/test/java/com/wavelabs/enrollment/service/test/GoogleGeoCodeServiceTest.java
+++ src/test/java/com/wavelabs/enrollment/service/test/GoogleGeoCodeServiceTest.java
@@ -1,20 +1,13 @@
 package com.wavelabs.enrollment.service.test;
 
-import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.anyString;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.runners.MockitoJUnitRunner;
-
 import com.wavelabs.enrollment.builder.DataBuilder;
-import com.wavelabs.enrollment.entity.User;
 import com.wavelabs.enrollment.service.GoogleGeoCode;
-
 
 @RunWith(MockitoJUnitRunner.class)
 public class GoogleGeoCodeServiceTest {
@@ -25,9 +18,8 @@ public class GoogleGeoCodeServiceTest {
 	@Test
 	public void testLatitudeAndLongitude() {
 		String zipCode = DataBuilder.getAddress().getZipCode();
-		String str[] = { "552.25", "85222" };
-//		Assert.assertEquals(str, googleGeoCode.getLatitudeLongitude(zipCode));
-//		when(googleGeoCode.getLatitudeLongitude(anyString()).thenReturn();
-		}
+		String str[] = { "17.3874364", "78.47217289999999" };
+		Assert.assertEquals(str, googleGeoCode.getLatitudeLongitude(zipCode));
+	}
 
 }
