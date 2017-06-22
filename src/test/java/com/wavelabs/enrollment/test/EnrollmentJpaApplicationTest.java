4071cdef8a352ab4510b56daea62c809f7750220
rajashekhar <shekhar.yedla@gmail.com>
2017-06-22 16:23:19 +0530

    coverage changes

--- src/test/java/com/wavelabs/enrollment/test/EnrollmentJpaApplicationTest.java
+++ src/test/java/com/wavelabs/enrollment/test/EnrollmentJpaApplicationTest.java
@@ -1,11 +1,19 @@
-package com.wavelabs.enrollment.test;
+/*package com.wavelabs.enrollment.test;
+
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.wavelabs.enrollment.EnrollmentJpaApplication;
 
 @SpringBootTest
  @RunWith(SpringRunner.class)
@@ -14,15 +22,16 @@ import org.springframework.test.context.junit4.SpringRunner;
 @PrepareForTest(SpringApplication.class)
 public class EnrollmentJpaApplicationTest {
 
-	/*@Test
+	@Test
 	public void mainTest() {
 
 		PowerMockito.mockStatic(SpringApplication.class);
 		when(SpringApplication.run(eq(EnrollmentJpaApplication.class), new String[] {}))
 				.thenReturn(mock(ConfigurableApplicationContext.class));
-	}*/
+	}
 	@Test
 	 public void contextLoads() {
 	 }
 
 }
+*/
